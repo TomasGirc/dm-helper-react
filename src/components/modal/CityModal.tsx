13f1706e74c79f19524a395bd9aa7e63fc79f849
TomasGir <gircius.t@gmail.com>
2024-08-14 22:27:17 +0300

    Add custom multi dropdown solution

--- src/components/modal/CityModal.tsx
+++ src/components/modal/CityModal.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import { ButtonComponent } from "../button/ButtonComponent";
+import MultiSelectDropdown from "../uxComponents/MultiSelectDropdown";
 
-const options = [
-  { id: 1, label: "Option 1" },
-  { id: 2, label: "Option 2" },
-  { id: 3, label: "Option 3" },
-  { id: 4, label: "Option 4" },
+const COUNTRIES = [
+  "Austria",
+  "Belgium",
+  "Croatia",
+  "Bulgaria",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Estonia",
+  "Finland",
+  "France",
+  "Germany",
+  "Greece",
+  "Hungary",
+  "Ireland",
+  "Italy",
+  "Latvia",
+  "Lithuania",
+  "Luxembourg",
+  "Malta",
+  "Netherlands",
+  "Poland",
+  "Portugal",
+  "Romania",
+  "Slovakia",
+  "Slovenia",
+  "Spain",
+  "Sweden",
+  "Ukraine",
 ];
 
 const CityModal = ({
@@ -14,21 +39,7 @@ const CityModal = ({
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
   const [name, setName] = React.useState<string>("");
-  const [selectedOptions, setSelectedOptions] = React.useState([]);
 
-  const handleSelect = (event) => {
-    const selectedValue = parseInt(event.target.value);
-    if (selectedOptions.includes(selectedValue)) {
-      setSelectedOptions(selectedOptions.filter((id) => id !== selectedValue));
-    } else {
-      setSelectedOptions([...selectedOptions, selectedValue]);
-    }
-  };
-
-  const getLabelById = (id) => {
-    const option = options.find((opt) => opt.id === id);
-    return option ? option.label : "";
-  };
   return (
     <>
       <form>
@@ -51,44 +62,17 @@ const CityModal = ({
             />
           </div>
           <div>
-            <label
-              htmlFor="region"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Select an option
-            </label>
-            <select
-              multiple
-              value={selectedOptions}
-              onChange={handleSelect}
-              className="w-full p-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              {options.map((option) => (
-                <option key={option.id} value={option.id}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
+            <MultiSelectDropdown
+              formFieldName={"countries"}
+              options={COUNTRIES}
+              onChange={(selectedCountries) => {
+                console.debug("selectedCountries", selectedCountries);
+              }}
+              prompt="Select one or more countries"
+            />
           </div>
         </div>
       </form>
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold">Selected Options:</h3>
-        <div className="mt-2 space-y-1">
-          {selectedOptions.length > 0 ? (
-            selectedOptions.map((id) => (
-              <div
-                key={id}
-                className="px-4 py-2 bg-blue-100 rounded-md text-blue-800"
-              >
-                {getLabelById(id)}
-              </div>
-            ))
-          ) : (
-            <p>No options selected.</p>
-          )}
-        </div>
-      </div>
       <ButtonComponent onClick={() => setShowModal(false)}>
         Close
       </ButtonComponent>
---
+++ src/components/uxComponents/MultiSelectDropdown.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import ClickOutside from "../ClickOutside";
+
+export default function MultiSelectDropdown({
+  formFieldName,
+  options,
+  onChange,
+  prompt = "Select one or more options",
+}) {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+  const optionsListRef = useRef(null);
+
+  const handleChange = (e) => {
+    const isChecked = e.target.checked;
+    const option = e.target.value;
+
+    const selectedOptionSet = new Set(selectedOptions);
+
+    if (isChecked) {
+      selectedOptionSet.add(option);
+    } else {
+      selectedOptionSet.delete(option);
+    }
+
+    const newSelectedOptions = Array.from(selectedOptionSet);
+
+    setSelectedOptions(newSelectedOptions);
+    onChange(newSelectedOptions);
+  };
+
+  const isSelectAllEnabled = selectedOptions.length < options.length;
+
+  const handleSelectAllClick = (e) => {
+    e.preventDefault();
+
+    const optionsInputs = optionsListRef.current.querySelectorAll("input");
+    optionsInputs.forEach((input) => {
+      input.checked = true;
+    });
+
+    setSelectedOptions([...options]);
+    onChange([...options]);
+  };
+
+  const isClearSelectionEnabled = selectedOptions.length > 0;
+
+  const handleClearSelectionClick = (e) => {
+    e.preventDefault();
+
+    const optionsInputs = optionsListRef.current.querySelectorAll("input");
+    optionsInputs.forEach((input) => {
+      input.checked = false;
+    });
+
+    setSelectedOptions([]);
+    onChange([]);
+  };
+
+  const checkHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleClickOutside = () => {
+    setIsChecked(false);
+  };
+
+  const ref = ClickOutside(handleClickOutside);
+
+  return (
+    <div ref={ref}>
+      <label className="relative">
+        <input
+          type="checkbox"
+          className="hidden peer"
+          checked={isChecked}
+          onChange={checkHandler}
+        />
+
+        <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform inline-flex border rounded px-5 py-2">
+          {prompt}
+          {selectedOptions.length > 0 && (
+            <span className="ml-1 text-blue-500">{`(${selectedOptions.length} selected)`}</span>
+          )}
+        </div>
+
+        <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 overflow-y-scroll">
+          <ul>
+            {options.map((option, i) => {
+              return (
+                <li key={option}>
+                  <label
+                    className={`flex whitespace-nowrap cursor-pointer px-2 py-1 transition-colors hover:bg-blue-100 [&:has(input:checked)]:bg-blue-200`}
+                  >
+                    <input
+                      type="checkbox"
+                      name={formFieldName}
+                      value={option}
+                      className="cursor-pointer"
+                      onChange={handleChange}
+                    />
+                    <span className="ml-1">{option}</span>
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </label>
+    </div>
+  );
+}
